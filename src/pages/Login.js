e5a86f7c509d9819b26996fb6804e92ff9896b0e
Michael Kielpinski <kielpinski.michael@gmail.com>
2022-08-18 15:08:19 -0700

    small change

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,16 +3,18 @@ import "../styles/login.scss"
 export default function Login(props) {
   const { handleLogIn } = props
   return (
-    <form id="loginForm" onSubmit={handleLogIn}>
-      <label>
-        Email:
-        <input required id="email" type="email" name="email"></input>
-      </label>
-      <label>
-        Password:
-        <input required id="password" type="password" name="password"></input>
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+    <div className="main container d-flex flex-d-col align-items-center">
+      <form id="loginForm" onSubmit={handleLogIn}>
+        <label>
+          Email:
+          <input required id="email" type="email" name="email"></input>
+        </label>
+        <label>
+          Password:
+          <input required id="password" type="password" name="password"></input>
+        </label>
+        <button type="submit">Log In</button>
+      </form>
+    </div>
   )
 }
--- src/pages/StaticHome.js
+++ src/pages/StaticHome.js
@@ -6,15 +6,41 @@ export default function HomeNotLoggedIn() {
       <article>
         <h3>Here is a list of some of the things you can do here</h3>
         <ol className='d-flex flex-d-col gap-1'>
-          <li>Create a user</li>
-          <li>Create a team</li>
-          <li>Request to join an existing team</li>
-          <li>Accept or reject user requests to join your team</li>
-          <li>Accept or reject requests to join someones team</li>
-          <li>Add project or get started on an existing one</li>
-          <li>Add tasks to a project</li>
-          <li>Set duedates for the tasks</li>
-          <li>Complete Tasks</li>
+          <li>
+            <s>
+              Create a user
+            </s>
+          </li>
+          <li>
+            Create a team
+          </li>
+          <li>
+            <s>
+              Request to join an existing team
+            </s>
+          </li>
+          <li>
+            <s>
+              Accept or reject user requests to join your team
+            </s>
+          </li>
+          <li>
+            <s>
+              Accept or reject requests to join someones team
+            </s>
+          </li>
+          <li>
+            Add project or get started on an existing one
+          </li>
+          <li>
+            Add tasks to a project
+          </li>
+          <li>
+            Set duedates for the tasks
+          </li>
+          <li>
+            Complete Tasks
+          </li>
         </ol>
       </article>
     </div>
