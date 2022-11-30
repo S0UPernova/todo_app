b2a201d99aab8313a03bf084f208d200ae7e90d1
Michael Kielpinski <kielpinski.michael@gmail.com>
2022-11-30 02:09:01 -0800

    this work?

--- src/utilities/Statics.js
+++ src/utilities/Statics.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const baseUrl = process.env.BASE_URL
-const origin = process.env.ORIGIN
+const baseUrl = process.env.REACT_APP_BASE_URL
+const origin = process.env.REACT_APP_ORIGIN
 // const origin = 'https://main.d28az3gs0u8gcy.amplifyapp.com/'
 // const baseUrl = 'http://localhost:3001';
 // const baseUrl = 'http://localhost:3002';
