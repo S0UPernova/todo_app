71621c0430bbd3a1f1a4438381c52c86daf7af54
Michael Kielpinski <kielpinski.michael@gmail.com>
2022-11-29 23:12:50 -0800

    trying changing baseURL

--- src/utilities/Statics.js
+++ src/utilities/Statics.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:3000';
+const baseUrl = 'https://main.d2qfl8zbqra4i8.amplifyapp.com/';
 // const baseUrl = 'http://localhost:3001';
 // const baseUrl = 'http://localhost:3002';
 // const baseUrl = 'https://bigbrotherapi.herokuapp.com';
