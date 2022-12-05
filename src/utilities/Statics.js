89e341fe5eadc5b421470b05e788d73cf8b5f72d
Michael Kielpinski <kielpinski.michael@gmail.com>
2022-12-05 02:34:58 -0800

    small change to rebulid the app

--- src/utilities/Statics.js
+++ src/utilities/Statics.js
@@ -1,5 +1,5 @@
-const baseUrl = process.env.REACT_APP_BASE_URL
-const origin = process.env.REACT_APP_ORIGIN
+const baseUrl = process.env.REACT_APP_BASE_URL ? process.env.REACT_APP_BASE_URL : 'http://localhost:3000'
+const origin = process.env.REACT_APP_ORIGIN ? process.env.REACT_APP_ORIGIN  : 'http://localhost:3001'
 // const origin = 'https://main.d28az3gs0u8gcy.amplifyapp.com/'
 // const baseUrl = 'http://localhost:3001';
 // const baseUrl = 'http://localhost:3002';
