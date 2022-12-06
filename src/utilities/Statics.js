013d3cd2a767cac0d17098e88dac7696eb4366dd
Michael Kielpinski <kielpinski.michael@gmail.com>
2022-12-05 21:09:40 -0800

    I think they will work now

--- src/utilities/Statics.js
+++ src/utilities/Statics.js
@@ -1,5 +1,6 @@
-const baseUrl = 'http://ec2-52-9-219-7.us-west-1.compute.amazonaws.com'
-const origin = 'https://main.d28az3gs0u8gcy.amplifyapp.com'
+const baseUrl = process.env.REACT_APP_BASE_URL
+const origin = process.env.REACT_APP_ORIGIN
+// const origin = 'https://main.d28az3gs0u8gcy.amplifyapp.com'
 // const origin = 'https://main.d28az3gs0u8gcy.amplifyapp.com/'
 // const baseUrl = 'http://localhost:3001';
 // const baseUrl = 'http://localhost:3002';
